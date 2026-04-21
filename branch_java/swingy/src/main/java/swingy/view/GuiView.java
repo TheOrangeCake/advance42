61e6a390479dd3db8fbe4dc8f462de04a2533405
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-04-21 15:03:37 +0200

    Setting page

--- branch_java/swingy/src/main/java/swingy/view/GuiView.java
+++ branch_java/swingy/src/main/java/swingy/view/GuiView.java
@@ -14,20 +14,18 @@ import java.util.function.Consumer;
 
 public class GuiView implements View {
     private final JFrame frame;
-    private final int height;
-    private final int width;
 
     public GuiView(int height, int width) {
         this.frame = new JFrame();
-        this.height = height;
-        this.width = width;
+        frame.setSize(width, height);
+        frame.setTitle("42 Swingy");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     @Override
     public void startPage(Consumer<MainMenuChoice> onChoice) {
-        JPanel container = new JPanel();
-        container.setBackground(Color.BLACK);
-        container.setLayout(new GridBagLayout());
+        JPanel panel = setLayout();
         GridBagConstraints gbc = new GridBagConstraints();
 
         JLabel gameTitle = new JLabel("42 Swingy");
@@ -38,34 +36,73 @@ public class GuiView implements View {
         gbc.gridy = 0;
         gbc.insets = new Insets(0, 0, 8, 0);
         gbc.anchor = GridBagConstraints.CENTER;
-        container.add(gameTitle, gbc);
+        panel.add(gameTitle, gbc);
 
-        JLabel divider = new JLabel("__________");
+        JLabel divider = new JLabel("*-*-*-*-*-*-*-*-*");
         divider.setForeground(Color.WHITE);
         divider.setFont(Typography.PARAGRAPH.getTypography());
         divider.setHorizontalAlignment(SwingConstants.CENTER);
         gbc.gridy++;
         gbc.insets = new Insets(0, 0, 40, 0);
-        container.add(divider, gbc);
+        panel.add(divider, gbc);
 
         gbc.insets = new Insets(12, 0, 12, 0);
         for (MainMenuChoice choice : MainMenuChoice.values()) {
             Button button = new Button(choice.getDescription());
             button.addActionListener(ae -> onChoice.accept(choice));
             gbc.gridy++;
-            container.add(button, gbc);
+            panel.add(button, gbc);
         }
-
-        frame.setSize(this.width, this.height);
-        frame.setTitle("42 Swingy");
-        frame.setContentPane(container);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        redoPage(panel);
     }
 
     @Override
     public void settingPage(Consumer<SettingMenuChoice> onChoice) {
+        JPanel panel = setLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        JLabel gameTitle = new JLabel("Setting");
+        gameTitle.setForeground(Color.WHITE);
+        gameTitle.setFont(Typography.H1.getTypography());
+        gameTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0, 0, 20, 0);
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(gameTitle, gbc);
+
+        JLabel divider1 = new JLabel("*-*-*-*-*-*-*-*-*");
+        divider1.setForeground(Color.WHITE);
+        divider1.setFont(Typography.PARAGRAPH.getTypography());
+        divider1.setHorizontalAlignment(SwingConstants.CENTER);
+        gbc.gridy++;
+        gbc.insets = new Insets(8, 0, 8, 0);
+        panel.add(divider1, gbc);
+
+        JLabel about = new JLabel("42 Swingy made by Nguyen NGUYEN (hoannguy) from 42 Lausanne");
+        about.setForeground(Color.WHITE);
+        about.setFont(Typography.H5.getTypography());
+        about.setHorizontalAlignment(SwingConstants.CENTER);
+        gbc.gridy++;
+        gbc.insets = new Insets(8, 0, 8, 0);
+        panel.add(about, gbc);
+
+        JLabel divider2 = new JLabel("*-*-*-*-*-*-*-*-*");
+        divider2.setForeground(Color.WHITE);
+        divider2.setFont(Typography.PARAGRAPH.getTypography());
+        divider2.setHorizontalAlignment(SwingConstants.CENTER);
+        gbc.gridy++;
+        gbc.insets = new Insets(8, 0, 40, 0);
+        panel.add(divider2, gbc);
+
+        gbc.insets = new Insets(12, 0, 12, 0);
+        for (SettingMenuChoice choice : SettingMenuChoice.values()) {
+            Button button = new Button(choice.getDescription());
+            button.addActionListener(ae -> onChoice.accept(choice));
+            gbc.gridy++;
+            panel.add(button, gbc);
+        }
+        redoPage(panel);
     }
 
     @Override
@@ -76,5 +113,18 @@ public class GuiView implements View {
     @Override
     public void stop() {
         frame.dispose();
+    }
+
+    private JPanel setLayout() {
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        panel.setLayout(new GridBagLayout());
+        return panel;
+    }
+
+    private void redoPage(JPanel panel) {
+        frame.setContentPane(panel);
+        frame.revalidate();
+        frame.repaint();
     }
 }
