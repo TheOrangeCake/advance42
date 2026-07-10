26f746db45d35367507863be68eee757c581e2b4
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-11 00:29:45 +0200

    start of Vaccine, parsing av

---
+++ branch_cyberSec/piscine/day_8_vaccine/src/setting.js
@@ -0,0 +1,77 @@
+import fs from "fs";
+
+const DEFAULT = "vaccine.sqlite";
+
+export class Setting {
+	x = "GET";
+	o = DEFAULT;
+	url = "";
+}
+
+export function parse_av(av) {
+	const setting = new Setting();
+
+	for (let i = 0; i < av.length; i++) {
+		let arg = av[i].trim();
+		switch (arg) {
+			case "-X":
+			case "-x":
+				const val_x = av[++i];
+				if (val_x === undefined) {
+						console.log(`[Error] Missing value for ${arg}`);
+						process.exit(1);
+				}
+				setting.x = parse_x(val_x.trim());
+				continue;
+			case "-O":
+			case "-o":
+				const val_o = av[++i];
+				if (val_o === undefined) {
+						console.log(`[Error] Missing value for ${arg}`);
+						process.exit(1);
+				}
+				setting.o = parse_o(val_o.trim());
+				continue;
+		}
+		if (arg.startsWith("http://")
+			|| arg.startsWith("https://")
+			|| arg.startsWith("www.")) {
+				setting.url = arg;
+				break;
+		}
+		console.log(`[Error] Unknow argument: ${av[i]}`);
+		process.exit(1);
+	}
+
+	if (setting.url === "") {
+		console.log(`[Error] Need an Url`);
+		process.exit(1);
+	}
+
+	if (setting.o === DEFAULT)
+		setting.o = parse_o(DEFAULT);
+
+	return setting;
+}
+
+function parse_x(x_opt) {
+	const x_opt_upper = x_opt.toUpperCase();
+	if (x_opt_upper !== "GET" && x_opt_upper !== "POST") {
+		console.log(`[Error] Unknow x option: ${x_opt}`);
+		process.exit(1);
+	}
+	return x_opt_upper;
+}
+
+// TODO: Initialize db here
+function parse_o(o_opt) {
+	try {
+		const fd = fs.openSync(o_opt, "w");
+		fs.closeSync(fd);	
+	} catch (err) {
+		console.log(`[Error] Cannot open output file '${o_opt}': ${err.code}`);
+		process.exit(1);
+	}
+
+	return o_opt;
+}
--- branch_cyberSec/piscine/day_8_vaccine/src/vaccine.js
+++ branch_cyberSec/piscine/day_8_vaccine/src/vaccine.js
@@ -1,1 +1,14 @@
-console.log("hey!");
+import {parse_av, Setting} from './setting.js';
+
+console.log("Launching Vaccine...");
+
+const av = process.argv.slice(2);
+
+if (av.length < 1) {
+	console.log("[Error] Need an URL");
+	process.exit(1);
+}
+
+const setting = parse_av(av);
+
+console.log(`[Setting] x: ${setting.x} | o: ${setting.o} | url: ${setting.url}`);
