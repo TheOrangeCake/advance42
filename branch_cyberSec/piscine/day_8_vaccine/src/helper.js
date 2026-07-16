03bd0f017dba82818b29f36cd02e501c0570bc12
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-16 18:05:52 +0200

    fix small bug

--- branch_cyberSec/piscine/day_8_vaccine/src/helper.js
+++ branch_cyberSec/piscine/day_8_vaccine/src/helper.js
@@ -2,14 +2,12 @@ import { dbClose } from "./database.js";
 import { printFatal, printError, printSuccess, printInfo } from "./logger.js";
 
 export function cleanUpFatal(db, ...a) {
-	type.toUpperCase();
 	dbClose(db);
 	printFatal(...a);
 	process.exit(1);
 }
 
 export function cleanUpInfo(db, ...a) {
-	type.toUpperCase();
 	dbClose(db);
 	printInfo(...a);
 	process.exit(1);
