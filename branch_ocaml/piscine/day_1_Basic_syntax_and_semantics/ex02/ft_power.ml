7be63cbad87ec8f346b1b1ff4c9ba3176228ab20
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-26 17:26:36 +0200

    day 1 ex02

---
+++ branch_ocaml/piscine/day_1_Basic_syntax_and_semantics/ex02/ft_power.ml
@@ -0,0 +1,19 @@
+exception Invalid_input
+let ft_power x y =
+	if (x = 0 && y = 0) || x < 0 || y < 0
+	then raise Invalid_input
+	else (
+		let rec power n =
+			if n = 0
+			then 1
+			else x * power (n - 1)
+		in power y
+	)
+
+
+let () =
+	print_endline(string_of_int(ft_power 2 4));
+	print_endline(string_of_int(ft_power 3 0));
+	print_endline(string_of_int(ft_power 0 5));
+	(try print_endline (string_of_int (ft_power 0 0))
+		with Invalid_input -> print_endline "Invalid_input")
