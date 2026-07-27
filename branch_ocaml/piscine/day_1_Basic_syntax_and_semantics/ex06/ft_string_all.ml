bee2cf66c19b7f8e1f940e7c8b7597a8e8dfd550
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-27 16:23:22 +0200

    day1 ex06

---
+++ branch_ocaml/piscine/day_1_Basic_syntax_and_semantics/ex06/ft_string_all.ml
@@ -0,0 +1,29 @@
+let ft_string_all f str =
+  let len = String.length str in
+  let rec apply i =
+    if i >= 0
+      then
+        let c = String.get str i in
+        if (f c) = true
+          then
+            apply (i - 1)
+          else
+            false
+      else
+        true
+  in apply (len - 1)
+
+(* let is_digit c =
+  c >= '0' && c <= '9'
+
+let () =
+  if (ft_string_all is_digit "123s45") = true
+    then
+      print_endline "Ok"
+    else
+      print_endline "Nope";
+  if (ft_string_all is_digit "12345") = true
+    then
+      print_endline "Ok"
+    else
+      print_endline "Nope" *)
