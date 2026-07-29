7e901381f25ba42af559242c9e6a40d34b896bbf
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-29 23:19:12 +0200

    day 2 ex03

--- branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex02/ackermann.ml
+++ branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex02/ackermann.ml
@@ -3,14 +3,16 @@ let rec ackermann m n =
     then -1
   else if m = 0
     then n + 1
-  else if m > 0 && n = 0
+  else if n = 0
     then ackermann (m - 1) 1
   else
     ackermann (m - 1) (ackermann m (n - 1))
 
-let () =
+(* let () =
         assert (ackermann 7 (-1) = -1);
         assert (ackermann (-1) 7 = -1);
         assert (ackermann 0 0 = 1);
         assert (ackermann 2 3 = 9);
         assert (ackermann 4 1 = 65533);
+        (* Below will stack overflow, wait MINUTES *)
+        (* print_int (ackermann 4 3); *) *)
