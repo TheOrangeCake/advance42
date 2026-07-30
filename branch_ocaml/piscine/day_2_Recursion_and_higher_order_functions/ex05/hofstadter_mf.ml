6c64741dd10bd9c86dda16d9556bd820d643b5a5
Nguyen <nguyennguyen.dev@protonmail.com>
2026-07-30 17:42:54 +0200

    day2 ex05

---
+++ branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex05/hofstadter_mf.ml
@@ -0,0 +1,20 @@
+let rec hfs_f n =
+  if n = 0
+    then 1
+  else if n < 0
+    then -1
+  else
+    n - hfs_m (hfs_f (n - 1))
+and hfs_m n =
+  if n = 0
+    then 0
+  else if n < 0
+    then -1
+  else
+    n - hfs_f (hfs_m (n - 1))
+
+(* let () =
+    assert (hfs_m 0 = 0);
+    assert (hfs_f 0 = 1);
+    assert (hfs_m 4 = 2);
+    assert (hfs_f 4 = 3); *)
