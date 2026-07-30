0b4e4c2758ad8d0951daf9fbd2c6c06494ceff7f
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-30 15:02:35 +0200

    save

--- branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex04/fibonacci.ml
+++ branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex04/fibonacci.ml
@@ -1,2 +1,14 @@
-let fibonacci n =
-  
+let rec fibonacci n =
+  if n < 0
+    then -1
+  else if n = 0 || n = 1
+    then n
+  else
+    fibonacci (n - 2) + fibonacci (n - 1)
+
+let () =
+    assert (fibonacci (-42) = -1);
+    assert (fibonacci 0 = 0);
+    assert (fibonacci 1 = 1);
+    assert (fibonacci 3 = 2);
+    assert (fibonacci 6 = 8);
