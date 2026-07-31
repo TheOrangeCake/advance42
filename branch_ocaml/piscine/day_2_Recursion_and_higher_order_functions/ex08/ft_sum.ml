678a2c26addd5b4c08815d51d751f2c9c2aa3d23
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-31 15:05:25 +0200

    day2 ex08

--- branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex08/ft_sum.ml
+++ branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex08/ft_sum.ml
@@ -6,9 +6,26 @@
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2026/07/31 14:10:23 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/07/31 14:11:18 by hoannguy         ###   ########.fr       *)
+(*   Updated: 2026/07/31 15:05:09 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let sigma f lower upper =
+let ft_sum f lower upper =
+  if upper < lower
+    then nan
+  else
+    let acc = 0. in
+    let rec sum low acc =
+      if low > upper
+        then acc
+      else
+        let result = ( +. ) acc (f low) in
+        sum (low + 1) result
+    in sum lower acc
+
+let squr i = float_of_int (i * i)
+
+let () =
+        assert (Float.is_nan (ft_sum squr 2 1));
+        assert (ft_sum squr 1 10 = 385.)
   
