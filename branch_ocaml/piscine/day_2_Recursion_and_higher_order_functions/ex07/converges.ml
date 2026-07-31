130ae1a5e0410c7f1def4072768c03579605128e
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-07-31 14:11:37 +0200

    day 2 ex07

--- branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex07/converges.ml
+++ branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex07/converges.ml
@@ -1,1 +1,35 @@
-let converges
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   converges.ml                                       :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2026/07/31 13:17:31 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/07/31 14:09:50 by hoannguy         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let rec converges f x n =
+  if n < 0
+    then false
+  else
+    let result = f x in
+    if result = x
+      then true
+    else
+      converges f result (n - 1)
+
+let div = fun x -> x / 2
+
+let double = fun x -> ( * ) 2 x
+
+let concat = fun str -> str ^ "42"
+
+(* let () =
+      assert (converges double 2 5 = false);
+      assert (converges div 2 3 = true);
+      assert (converges div 2 2 = true);
+      assert (converges div 8 2 = false);
+      assert (converges div 2 (-2) = false);
+      assert (converges concat "test" 2 = false) *)
--- branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex08/ft_sum.ml
+++ branch_ocaml/piscine/day_2_Recursion_and_higher_order_functions/ex08/ft_sum.ml
@@ -1,28 +1,14 @@
 (* ************************************************************************** *)
 (*                                                                            *)
 (*                                                        :::      ::::::::   *)
-(*   iter.ml                                            :+:      :+:    :+:   *)
+(*   ft_sum.ml                                          :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
-(*   Created: 2026/07/31 12:52:17 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/07/31 13:20:39 by hoannguy         ###   ########.fr       *)
+(*   Created: 2026/07/31 14:10:23 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/07/31 14:11:18 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let rec iter f x n =
-  if n < 0
-    then -1
-  else if n = 0
-    then x
-  else
-    iter f (f x) (n - 1)
-
-let multi = fun x -> x * x
-let multi2 = fun x -> x * 2
-
-(* let () =
-    assert (iter multi 2 4 = 65536);
-    assert (iter multi2 2 4 = 32);
-    assert (iter multi 2 (-10) = -1);
-    assert (iter multi 2 0 = 2); *)
+let sigma f lower upper =
+  
