99791ab455f5218b7165244d8d4a30759eaefe59
Nguyen <nguyennguyen.dev@protonmail.com>
2026-08-02 22:31:01 +0200

    day 3 ex03

--- branch_ocaml/piscine/day_3_Pattern_matching_and_data_types/ex03/sequence.ml
+++ branch_ocaml/piscine/day_3_Pattern_matching_and_data_types/ex03/sequence.ml
@@ -6,32 +6,50 @@
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2026/08/02 17:40:09 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/08/02 18:58:58 by hoannguy         ###   ########.fr       *)
+(*   Updated: 2026/08/02 22:30:01 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
+
+let string_of_digit = function
+  | 1 -> "1"
+  | 2 -> "2"
+  | 3 -> "3"
+  | 4 -> "4"
+  | 5 -> "5"
+  | 6 -> "6"
+  | 7 -> "7"
+  | 8 -> "8"
+  | 9 -> "9"
+  | 0 -> "0"
+  | _ -> ""
 
 let sequence n =
   if n <= 0
     then ""
   else
     let rec loop x lst =
-      if x > n
+      if x >= n
         then lst
       else
-        let rec iter_list lst count acc =
+        let rec iter_list lst count =
           match lst with
-          | [] -> loop (x + 1) acc
-          | [a] -> count :: a :: acc
+          | [] -> []
+          | [a] -> a :: count :: []
           | a :: (b :: _ as rest) ->
             if a = b
               then
-                iter_list rest (count + 1) acc
+                iter_list rest (count + 1)
               else
-                iter_list rest 1 (count :: a :: acc)
+                a :: count :: iter_list rest 1
 
-        in iter_list lst 1 []
-
-      in loop 1 [1]
+        in loop (x + 1) (iter_list lst 1)
+      
+      in let rec to_string lst acc =
+        match lst with
+        | [] -> acc
+        | h :: t -> to_string t (string_of_digit h ^ acc)
+        
+      in to_string (loop 1 [1]) ""
 
 let () =
     assert (sequence 1 = "1");
