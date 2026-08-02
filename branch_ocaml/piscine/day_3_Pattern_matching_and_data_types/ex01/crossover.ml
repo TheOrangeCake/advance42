548dad500e9aba878a429c843e447c10f84a6e3e
Nguyen <nguyennguyen.dev@protonmail.com>
2026-08-02 11:03:44 +0200

    day3 ex01

---
+++ branch_ocaml/piscine/day_3_Pattern_matching_and_data_types/ex01/crossover.ml
@@ -0,0 +1,42 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   crossover.ml                                       :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2026/08/02 08:44:37 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/08/02 11:03:32 by hoannguy         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let crossover lst1 lst2 =
+  let rec is_mem x lst =
+    match lst with
+    | [] -> false
+    | y :: tail -> x = y || is_mem x tail
+  
+  in let rec rev lst acc =
+    match lst with
+    | [] -> acc
+    | h :: t -> rev t (h :: acc)
+
+  in let rec loop lst1 acc =
+    match lst1 with
+    | [] -> rev acc []
+    | x :: rest ->
+        if is_mem x lst2 && not (is_mem x acc)
+          then
+            loop rest (x :: acc)
+          else
+            loop rest acc
+
+  in loop lst1 []
+
+let () =
+        assert (crossover [] [1; 2] = []);
+        assert (crossover [1; 2] [] = []);
+        assert (crossover [1; 2] [1; 3] = [1]);
+        assert (crossover [1; 2; 3; 3] [1; 3; 1; 2] = [1; 2; 3]);
+        assert (crossover ["42"; "Lausanne"; "42"; "Lausanne"] ["42"; "Lausanne"; "42"; "!"] = ["42"; "Lausanne"])
+ 
