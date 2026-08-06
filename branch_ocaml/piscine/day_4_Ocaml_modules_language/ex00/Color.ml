2ea2e23bd868afcadbaf6af4e089d9e0c039e3e5
Nguyen <nguyennguyen.dev@protonmail.com>
2026-08-06 19:05:51 +0200

    save

---
+++ branch_ocaml/piscine/day_4_Ocaml_modules_language/ex00/Color.ml
@@ -0,0 +1,27 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   Color.ml                                           :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2026/08/05 15:31:18 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/08/06 19:05:27 by hoannguy         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+type t = Spade | Heart | Diamond | Club
+
+let all = [Spade; Heart; Diamond; Club]
+
+let toString = function
+  | Spade -> "S"
+  | Heart -> "H"
+  | Diamond -> "D"
+  | Club -> "C"
+
+let toStringVerbose = function
+  | Spade -> "Spade"
+  | Heart -> "Heart"
+  | Diamond -> "Diamond"
+  | Club -> "Club"
