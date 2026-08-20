540cee73d60f6b909d64557f1fe38636a91692c5
Nguyen <nguyennguyen.dev@protonmail.com>
2026-08-21 00:29:45 +0200

    day5 ex01

--- branch_ocaml/piscine/day_5_Imperative_features/ex01/ft_ref.ml
+++ branch_ocaml/piscine/day_5_Imperative_features/ex01/ft_ref.ml
@@ -6,7 +6,7 @@
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2026/08/19 23:36:06 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/08/20 19:11:16 by hoannguy         ###   ########.fr       *)
+(*   Updated: 2026/08/21 00:29:20 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -27,4 +27,37 @@ module Ref : REF = struct
   let bind (v_ref : 'a ft_ref) (f : ('a -> 'b ft_ref)) = get v_ref |> f
 end
 
-let increase v = Ref.return |> Ref.set v
+let intToFloat v = Ref.return (float_of_int v)
+
+let () =
+  let ref1 = Ref.return 42 in
+  print_string "Creating new ref1 of value -> ";
+  print_int (Ref.get ref1);
+  print_char '\n';
+
+  let ref2 = Ref.return 42 in
+  print_string "Creating new ref2 of value -> ";
+  print_int (Ref.get ref2);
+  print_char '\n';
+
+  print_string "Is ref1 equal to ref2 structurely? -> ";
+  if (ref1 = ref2) then print_endline "True" else print_endline "False";
+  
+  print_string "Is ref1 equal to ref2 physicaly? -> ";
+  if (ref1 == ref2) then print_endline "True" else print_endline "False";
+
+  print_string "Mutate ref1 value to 1337 -> ";
+  Ref.set ref1 1337;
+  print_int (Ref.get ref1);
+  print_char '\n';
+
+  print_string "Is ref1 still equal to ref2 structurely? -> ";
+  if (ref1 = ref2) then print_endline "True" else print_endline "False";
+
+  print_string "Bind ref1 with intToFloat function -> ";
+  print_float (Ref.bind ref1 intToFloat |> Ref.get);
+  print_char '\n';
+
+  print_string "What is ref1 value now -> ";
+  print_int (Ref.get ref1);
+  print_char '\n';
