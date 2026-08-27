4e9e83eef767417a642e9dcef78d4031fd4df7db
Nguyen <nguyennguyen.dev@protonmail.com>
2026-08-28 00:21:21 +0200

    day6 ex00

---
+++ branch_ocaml/piscine/day_6_Functors/ex00/ex00.ml
@@ -0,0 +1,18 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   ex00.ml                                            :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2026/08/27 23:59:02 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/08/28 00:08:15 by hoannguy         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+module StringSet = Set.Make(String)
+
+let () =
+  let set = List.fold_right StringSet.add [ "foo"; "bar"; "baz"; "qux" ] StringSet.empty in
+  StringSet.iter print_endline set;
+  print_endline (StringSet.fold ( ^ ) set "")
