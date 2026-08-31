45d56637f68fde4fbd3c6aa1fb60c090c62a1939
Nguyen <nguyennguyen.dev@protonmail.com>
2026-09-01 00:09:58 +0200

    day7 ex01

--- branch_ocaml/piscine/day_7_OOP_1/ex01/main.ml
+++ branch_ocaml/piscine/day_7_OOP_1/ex01/main.ml
@@ -1,21 +1,21 @@
 (* ************************************************************************** *)
 (*                                                                            *)
 (*                                                        :::      ::::::::   *)
-(*   people.ml                                          :+:      :+:    :+:   *)
+(*   main.ml                                            :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
-(*   Created: 2026/08/31 18:58:20 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/08/31 23:32:51 by hoannguy         ###   ########.fr       *)
+(*   Created: 2026/08/31 18:58:16 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/09/01 00:06:55 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-class people (name: string) =
-  object (self)
-  val name = name
-  val hp = 100
-  method to_string = "Name: " ^ name ^ " | Hp: " ^ string_of_int hp
-  method talk = print_endline ("I'm " ^ name ^ "! Do you know the Doctor?")
-  method die = print_endline "Aaaarghh!"
-  initializer print_endline ("Me, " ^ name ^ ", has risen! " ^ self#to_string)
-end
+let () =
+  let sidekick = new People.people "Pookie" in
+  let doc = new Doctor.doctor "Frimousse" 15 sidekick in
+  print_endline doc#to_string;
+  doc#talk;
+  doc#travel_in_time 1996 2026;
+  print_endline doc#to_string;
+  doc#use_sonic_screwdriver
+  (* doc#regenerate *)
