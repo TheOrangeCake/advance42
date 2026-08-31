d22c7870068f163d1a5f4440add37e69f708e65d
Nguyen <nguyennguyen.dev@protonmail.com>
2026-08-31 23:25:02 +0200

    day7 ex00

---
+++ branch_ocaml/piscine/day_7_OOP_1/ex00/main.ml
@@ -0,0 +1,17 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   main.ml                                            :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2026/08/31 18:58:16 by hoannguy          #+#    #+#             *)
+(*   Updated: 2026/08/31 23:20:39 by hoannguy         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let () =
+  let people1 = new People.people "Pookie" in
+  print_endline people1#to_string;
+  people1#talk;
+  people1#die
