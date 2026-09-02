58bc8b132201badd7a7411000cb5c9d90f97208a
Nguyen <nguyennguyen.dev@protonmail.com>
2026-09-02 15:14:36 +0200

    day7 ex03

--- branch_ocaml/piscine/day_7_OOP_1/ex03/army.ml
+++ branch_ocaml/piscine/day_7_OOP_1/ex03/army.ml
@@ -6,7 +6,20 @@
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2026/09/01 23:11:29 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/09/01 23:11:29 by hoannguy         ###   ########.fr       *)
+(*   Updated: 2026/09/02 15:13:41 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
+class ['a] army =
+  object
+    val mutable members = ([]: 'a list)
+
+    method add x = members <- x :: members
+
+    method delete =
+      match members with
+      | [] -> print_endline "Army has no member"
+      | hd :: tl ->
+        (* print_endline ("Discharged: " ^ hd#to_string); *)
+        members <- tl
+end
--- branch_ocaml/piscine/day_7_OOP_1/ex03/main.ml
+++ branch_ocaml/piscine/day_7_OOP_1/ex03/main.ml
@@ -6,53 +6,45 @@
 (*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2026/08/31 18:58:16 by hoannguy          #+#    #+#             *)
-(*   Updated: 2026/09/01 23:00:57 by hoannguy         ###   ########.fr       *)
+(*   Updated: 2026/09/02 15:10:10 by hoannguy         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let () =
-  let human = new People.people "Moussy" in
-  let sidekick = new People.people "Pookie" in
-  let doc = new Doctor.doctor "Frimousse" 15 sidekick in
-  let monster = new Dalek.dalek in
+  let human_names = ["human1"; "human2"; "human3"] in
+  let doctor_names = ["doctor1"; "doctor2"; "doctor3"] in
+  let sidekick_names = ["side1"; "side2"; "side3"] in
 
-  print_endline "\n===== EVERYONE SPEAKS =====";
-  human#talk;
-  sidekick#talk;
-  doc#talk;
-  monster#talk;
+  let army_human = new Army.army in
+  let army_doctor = new Army.army in
+  let army_dalek = new Army.army in
+  let len = List.length human_names in
 
-  print_endline "\n===== THE DOCTOR ARRIVES =====";
-  doc#travel_in_time 1986 2026;
-  doc#use_sonic_screwdriver;
-  print_endline doc#to_string;
-
-  print_endline "\n===== THE DALEK EXTERMINATES THE HUMAN =====";
-  monster#exterminate human;
-  print_endline human#to_string;
-  print_endline monster#to_string;
-  human#talk;
-
-  print_endline "\n===== DOCTOR VS DALEK =====";
-  let rec battle round =
-    if not monster#is_alive then print_endline "The Doctor wins!"
-    else if not doc#is_alive then print_endline "The Dalek wins! EXTERMINATE!"
-    else if round > 5 then print_endline "The Time War drags on..."
+  print_endline "\n===== RAISING THE ARMIES =====";
+  let rec build i =
+    if i >= len then ()
     else begin
-      print_endline ("--- Round " ^ string_of_int round ^ " ---");
-      if round = 3 
-        then begin
-          print_endline "Exterminate the sidekick!";
-          monster#exterminate sidekick
-        end;
-      monster#take_damage 30;
-      if monster#is_alive then begin
-        monster#talk;
-        doc#take_damage 15
-      end;
-      print_endline monster#to_string;
-      print_endline doc#to_string;
-      print_char '\n';
-      battle (round + 1)
+      let human = new People.people (List.nth human_names i) in
+      army_human#add human;
+
+      let sidekick = new People.people (List.nth sidekick_names i) in
+      army_doctor#add (new Doctor.doctor (List.nth doctor_names i) 15 sidekick);
+
+      let dalek = new Dalek.dalek in
+      army_dalek#add dalek;
+
+      print_endline "";
+      build (i + 1)
     end
-  in battle 1
+  in build 0;
+
+  print_endline "\n===== DISBANDING THE ARMIES =====";
+  let rec destroy i =
+    if i > len then ()
+    else begin
+      army_human#delete;
+      army_doctor#delete;
+      army_dalek#delete;
+      destroy (i + 1)
+    end
+  in destroy 0
